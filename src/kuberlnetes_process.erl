e989a4ce6963be4dd451346fb2272739d1aee8cd
Philip Cristiano <git@philipcristiano.com>
2019-11-07 07:32:19 -0500

    Remove lager
    It wasn't defined as a dep here and is preventing compilation

--- src/kuberlnetes_process.erl
+++ src/kuberlnetes_process.erl
@@ -1,5 +1,4 @@
 -module(kuberlnetes_process).
--compile({parse_transform, lager_transform}).
 
 -export([run/3]).
 
@@ -18,7 +17,7 @@ loop(Port, Acc) ->
     receive
         {Port, {exit_status, Status}} -> {Status, Acc};
         {Port, {data, Bytes}} -> loop(Port, Acc ++ Bytes);
-        Msg -> ok = lager:debug("Unhandled loop message ~p", [Msg])
+        Msg -> {error, unhandled_message, Msg}
     end.
 
 % output_to_var({data, {eol, Data}}) ->
